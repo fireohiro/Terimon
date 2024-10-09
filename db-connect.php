c50f17467a7a002465701b14b59e01af65d814f7
fireohiro <148302631+fireohiro@users.noreply.github.com>
2024-10-09 10:16:00 +0900

    環境

---
+++ db-connect.php
@@ -0,0 +1,9 @@
+<?php
+    const SERVER = 'mysql311.phy.lolipop.lan';
+    const DBNAME = 'LAA1517450-terimon';
+    const USER = 'LAA1517450';
+    const PASS = 'pass0423';
+
+    $connect = 'mysql:host='.SERVER.';dbname='.DBNAME.';charset=utf8';
+    $pdo=new PDO($connect,USER,PASS);
+?>
