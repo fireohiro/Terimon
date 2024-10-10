3b58e91af64da7b6ac48c77f839f0cbfceb02dfe
ぽえぽえぽえぽえ <120158903+poepoepoepoe@users.noreply.github.com>
2024-10-10 12:12:08 +0900

    add

---
+++ sorce/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=RocknRoll+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=BIZ+UDPMincho:wght@400;700&display=swap" rel="stylesheet">
+    <title>テリモンのわんだーらんど</title>
+</head>
+<body>
+    <h1>テリモンのわんだーらんど</h1>
+    <div class="field">
+    <div class="font1">アカウント名：<input type="text"></div>
+    <div class="font1">パスワード：　<input type="password"></div>
+    <button type="button" onclick="location.href='login-confirm.php'"><div class="font2">ログイン</div></button><br>
+    <button type="button" onclick="location.href='b.html'"><div class="font2">新規作成</div></button>
+    </div>
+</body>
+</html>
