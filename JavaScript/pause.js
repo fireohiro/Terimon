5b8864e5c6b3b20169e66be17274ea1535d3b4e3
TK0606 <2201137@s.asojuku.ac.jp>
2024-10-22 11:05:54 +0900

    pause

--- JavaScript/pause.js
+++ JavaScript/pause.js
@@ -1,10 +1,10 @@
-export function createPause(gameStatus,playerStatus){
+export function createPause(gameStatus,playerStatus,config){
     this.input.keyboard.on('keydown',(event) => {
         if(event.key === 'Escape'){
             pauseStart.call(this,gameStatus);
         }
     });
-    menuBar(this,playerStatus);
+    menuBar(this,playerStatus,config);
 }
 
 function pauseStart(gamestatus){
@@ -19,19 +19,26 @@ function pauseStart(gamestatus){
     }
 }
 
-async function menuBar(loader,playerStatus){
+async function menuBar(loader,playerStatus,config){
     //メニューのサイズを設定
+    const menuWidth = config.width * 0.20;
+    const menuHeight = config.height * 0.70;
+
+    // メニュー背景を作成し、左に少しスペースを開ける
+    const menuBackground = loader.add.rectangle(menuWidth * -0.5, 0, menuWidth, menuHeight, 0x93C572, 0.8);
+    menuBackground.setStrokeStyle(4, 0xFFFDD0); // 緑枠
 
     //テキスト（ボタンを設定）の例
-    const itemtext = loader.add.text(0.5,-100,'アイテム',{fontSize:'18px'});
-    const geartext = loader.add.text(0.5,-75,'装備',{fontSize:'18px'});
-    const statustext = loader.add.text(0.5,-50,'ステータス',{fontSize:'18px'});
-    const savetext = loader.add.text(0.5,-25,'アイテム',{fontSize:'18px'});
-    const logouttext = loader.add.text(0.5,0,'タイトルへ戻る',{fontSize:'18px'});
+    const itemtext = loader.add.text(menuWidth*-0.5/2,-100,'アイテム',{fontSize:'18px'});
+    const geartext = loader.add.text(menuWidth*-0.5/2,-75,'装備',{fontSize:'18px'});
+    const statustext = loader.add.text(menuWidth*-0.5/2,-50,'ステータス',{fontSize:'18px'});
+    const savetext = loader.add.text(menuWidth*-0.5/2,-25,'セーブ',{fontSize:'18px'});
+    const logouttext = loader.add.text(menuWidth*-0.5/2,0,'タイトルへ戻る',{fontSize:'18px'});
 
     //クリックイベント
     itemtext.setInteractive().on('pointerdown',()=>{
         //アイテム使用用のプログラムの関数を呼び出す
+        
     });
     geartext.setInteractive().on('pointerdown',()=>{
         //装備用のプログラムの関数を呼び出す
@@ -47,11 +54,15 @@ async function menuBar(loader,playerStatus){
     });
 
     //所持金欄の大きさ設定
+    const goldBarHeight = menuHeight*0.25
+    const Moneybar = loader.add.rectangle(menuWidth*-0.5,menuHeight / 2 + goldBarHeight / 2+52,menuWidth,goldBarHeight,0x93C572,0.8);
+    Moneybar.setStrokeStyle(4,0xFFFDD0);
 
     //所持金のテキスト
     const moneytext = loader.add.text(
         //文字の設定兼表示内容
-        10,10,//表示位置ｘとｙ
+        Moneybar.x-Moneybar.displayWidth/2,
+        Moneybar.y-Moneybar.displayHeight/2,//表示位置ｘとｙ
         //表示文字と文字の設定
         `所持金\n${playerStatus.money}`,
         {fontSize:'18px',fill:'#fff'}
