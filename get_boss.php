08c89111ce0457a8ea7d0e7493a0853294468119
fireohiro <148302631+fireohiro@users.noreply.github.com>
2024-10-26 23:09:43 +0900

    バトルプログラム25%

--- get_boss.php
+++ get_boss.php
@@ -1,29 +1,34 @@
 <?php
     require 'db-connect.php';
     $pdo=new PDO($connect,USER,PASS);
-    //雑魚をランダムで３体出現させる
+    //ボスをランダムで３体出現させる
     $sql=$pdo->query("select * from monster where bunrui = 'ボス' order by rand() limit 3");
     $result = $sql->fetchAll();
     $enemyData = [];
     foreach($result as $monster){
+        $maxhp = rand($monster['hp_kagen'],$monster['hp_zyougen']);
+        $maxmp = rand($monster['mp_kagen'],$monster['mp_zyougen']);
         //モンスターのステータスを決めて格納します
         $enemy = [
             'id' => $monster['monster_id'],
             'name' => $monster['monster_name'],
             'gazou' => $monster['gazou'],
             'resist' => $monster['resist'],
-            'hp' => rand($monster['hp_kagen'],$monster['hp_zyougen']),
-            'mp' => rand($monster['mp_kagen'],$monster['mp_zyougen']),
+            'hp' => $maxhp,
+            'nokori_hp' => $maxhp,
+            'mp' => $maxmp,
+            'nokori_mp' => $maxmp,
             'pow' => rand($monster['pow_kagen'],$monster['pow_zyougen']),
             'def' => rand($monster['def_kagen'],$monster['def_zyougen']),
             'speed' => rand($monster['speed_kagen'],$monster['speed_zyougen']),
             'lack' => $monster['lack'],
             'experience' => $monster['experience'],
-            'drop_money' => $monster['drop_money']
+            'drop_money' => $monster['drop_money'],
+            'type' => 'enemy'
         ];
         $enemyData[] = $enemy;
     }
-    //JSON毛k式で返す
+    //JSON形式で返す
     header('Content-Type:application/json');
     echo json_encode($enemyData);
 ?>
