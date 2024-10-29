4555500dc5ca59cad8603da69f498f8f553d1912
TK0606 <2201137@s.asojuku.ac.jp>
2024-10-29 13:39:29 +0900

    henkou

--- JavaScript/status.js
+++ JavaScript/status.js
@@ -1,9 +1,9 @@
 export function createStatusScreen(loader,gameStatus, playerStatus,friend1Status,friend2Status,friend3Status, config) {
     const statusWidth = config.width * 0.60;
-    const statusHeight = config.height * 0.70;
+    const statusHeight = config.height * 0.95;
 
     // ステータス画面の背景
-    const statusBackground = loader.add.rectangle(statusWidth / 2, config.height / 2, statusWidth, statusHeight, 0xFFFFE0, 0.8);
+    const statusBackground = loader.add.rectangle(config.width * 0.3 + 10, 2, statusWidth, statusHeight, 0xFFFFE0, 0.8);
     statusBackground.setStrokeStyle(4, 0x000000);
 
     // プレイヤーのステータス情報を表示
