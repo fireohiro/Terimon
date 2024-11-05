efd63a20c6c4d710bfe5fe4f1fa5c98d5d68c9d4
fireohiro <148302631+fireohiro@users.noreply.github.com>
2024-11-05 13:43:09 +0900

    修正

--- JavaScript/save.js
+++ JavaScript/save.js
@@ -1,21 +1,21 @@
+let saveContainer;
 export function saveEvent(gameStatus){
     gameStatus.saveflg = !gameStatus.saveflg;
     saveContainer.setVisible(gameStatus.saveflg);
 }
 
-export async function saveGame(loader,playerStatus,config,gameStatus,friend1Status,friend2Status,friend3Status){
+export async function saveGame(scene,playerStatus,config,gameStatus,friend1Status,friend2Status,friend3Status){
     //メニューのサイズを設定
     const saveWidth = config.width * 0.6;
     const saveHeight = config.height * 0.95;
 
     // メニュー背景を作成し、左に少しスペースを開ける
-    const saveback = loader.add.rectangle(config.width * 0.3 + 10, 2, saveWidth, saveHeight, 0xFFFFFF, 0.8);
+    const saveback = scene.add.rectangle(config.width * 0.3 + 10, 2, saveWidth, saveHeight, 0xFFFFFF, 0.8);
     saveback.setStrokeStyle(2,0x000000); // 緑枠
-    saveback.setRadius(10);
 
     //テキスト（ボタンを設定）の例
-    const savetext = loader.add.text(config.width * 0.3 + 10 + saveWidth/2,saveHeight / -2 - 50,'はい',{fontSize:'18px'});
-    const backtext = loader.add.text(config.width * 0.3 + 10 + saveWidth/2,saveHeight / -2,'いいえ',{fontSize:'18px'});
+    const savetext = scene.add.text(config.width * 0.3 + 10 + saveWidth/2,saveHeight / -2 - 50,'はい',{fontSize:'18px'});
+    const backtext = scene.add.text(config.width * 0.3 + 10 + saveWidth/2,saveHeight / -2,'いいえ',{fontSize:'18px'});
     //クリックイベント
     savetext.setInteractive().on('pointerdown',async()=>{
             //保存データ準備
@@ -48,7 +48,7 @@ export async function saveGame(loader,playerStatus,config,gameStatus,friend1Stat
     backtext.setInteractive().on('pointerdown',()=>{
          saveEvent(gameStatus);
     });
-    saveContainer.loader.add.container(0,0,[saveback,sevetext,backtext]);
+    saveContainer = scene.add.container(0,0,[saveback,savetext,backtext]);
     saveContainer.setVisible(false);
     saveContainer.setDepth(7);
 }
