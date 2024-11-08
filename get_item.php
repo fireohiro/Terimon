99bce7e82d583d3a73890dd32fbf0b95bdd4a7a3
ぽえぽえぽえぽえ <120158903+poepoepoepoe@users.noreply.github.com>
2024-11-08 10:47:45 +0900

    +header

--- get_item.php
+++ get_item.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    header('Content-Type:application/json');
     require 'db-connect.php';
     $pdo = new PDO($connect,USER,PASS);
     $data = json_decode(file_get_contents("php://input"),true);
@@ -8,9 +9,9 @@
     $items = [];
     $sql=$pdo->prepare(
         'SELECT i.*, gi.su 
-         FROM item AS i 
-         INNER JOIN get_item AS gi ON i.item_id = gi.item_id 
-         WHERE gi.account_id = ? AND gi.su >= 1');
+        FROM item AS i 
+        INNER JOIN get_item AS gi ON i.item_id = gi.item_id 
+        WHERE gi.account_id = ? AND gi.su >= 1');
     $sql->execute([$account_id]);
     $items = $sql->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode(['items' => $items]);
